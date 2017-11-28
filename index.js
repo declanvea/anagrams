f94ac383fb562855ba1f9e24d55dd79eabc047f8
Declan Vea <declanpvea@gmail.com>
2017-11-28 15:04:05 -0500

    sorted letters, not solution

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // P1: find all the words that are anagrams of “least.”  (There will be 4 words in this set, including “least” itself).
-
 let value1 = 'least';
 function sortValue(str) {
-    return str.split('').sort().join('');
+    return str.toLowerCase().split('').sort().join('').trim();
 }
 let valueSort1 = sortValue(value1);
 let answer1 = words.filter(function(x){
@@ -12,7 +11,6 @@ console.log(answer1);
 
 // P2: The array ["lair", "liar", "rail"] is a set of three words that are all anagrams of each other - that is, they all consist of exactly the same letters. Find all the sets of 5 or more words within words.json that are anagrams of each other.
 // Note: You should not need to compare every word against every other!
-
 let dictionary = {};
 for (let i = 0; i < words.length; i++) {
   let word = words[i];
@@ -23,7 +21,6 @@ for (let i = 0; i < words.length; i++) {
     dictionary[sort] = [word];
   }
 }
-
 let dictionary2 = {};
 for (let prop in dictionary) {
   if(dictionary[prop].length >= 5){
@@ -32,29 +29,46 @@ for (let prop in dictionary) {
 }
 console.log(dictionary2);
 
-
-
 // P3: Allow the user to enter text in a text field and search for all the words that are anagrams of it.
 let input = document.getElementById('input');
-function sortValue(str) {
-    return str.split('').sort().join('');
-}
 function handleWord(){
   let valueSort = sortValue(input.value);
-  let answer1 = words.filter(function(x){
+  let answer3 = words.filter(function(x){
         return sortValue(x) == valueSort;
-  });
-  console.log(answer1);
+});
+  console.log(answer3);
 };
-
-
 
 // P4: Allow the user to enter a word or phrase in a text field and search for two-word anagrams (ignoring spaces). Some examples:
 // 	declan vea
 // 	[ "aden", "dane", "dean", "edna" ] + [ "calve" ]
 // 	[ "and", "dan", "dna" ] + [ "cleave" ]
 // 	(these are examples, your program should find many more anagram combinations)
-//
+let input2 = document.getElementById('input2');
+let objectKeys = Object.keys(dictionary);
+console.log(objectKeys);
+
+function handleWord2(){
+  let sortedInput = sortValue(input2.value);
+  console.log(sortedInput);
+  console.log(sortedInput.length);
+  let filterKeys = [];
+  for (let i = 0; i < objectKeys.length; i++) {
+    let objectI = objectKeys[i];
+    for (let j = 0; j < objectI.length; j++) {
+      let objectIJ = objectI[j];
+      if(sortedInput.indexOf(objectIJ) !== -1){
+        filterKeys.push(objectIJ);
+      } else {
+        j++;
+      }
+    }
+  }
+  console.log(filterKeys);
+};
+
+
+
 // P5: Search for three-word anagrams of a user-provided phrase. For example:
 // 	maricris tejada
 // 	[ "dare", "dear", "erda", "read" ] + [ "jam" ] + [ "satiric" ]
